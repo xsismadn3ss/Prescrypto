9d366cbacb6688aa1621af1e678d894c4c25cfd7
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-12 01:20:07 -0600

    edit settings

--- DApp/DApp/settings.py
+++ DApp/DApp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default = "lkflekvffajskfeecasee242")
+SECRET_KEY = "django-insecure-q5#!0b4e26^ov&w)c24ppcj0)e5&+km@fv3344*om*6@ru6%)2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
